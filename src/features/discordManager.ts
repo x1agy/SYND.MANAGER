5c11664ef931b0c0623cd6c10058c43ffb58e91f
x1agy <skizzzgrand@gmail.com>
2026-08-14 20:32:02 +0300

    feat: added id`s

--- src/features/discordManager.ts
+++ src/features/discordManager.ts
@@ -58,8 +58,7 @@ const discordInteractionHandler = async (
     const usernames = [...members.values()]
       .map((member) => member.user)
       .filter((user) => Boolean(user) && !user.bot)
-      .map((user) => user.username)
-      .sort((a, b) => a.localeCompare(b));
+      .map((user) => `${user.username} ${user.id}`);
 
     const fileContent = usernames.join('\n');
 
